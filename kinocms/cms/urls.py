f27b3e10fc839159c34761961bc614d67fa5923d
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-11-26 19:54:35 +0200

    Created views and URLs for main CMS pages

--- kinocms/cms/urls.py
+++ kinocms/cms/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.index)
+    path('logout/', logout_user, name='logout'),
+    path('statistics/', statistics, name='statistics'),
+    path('banners/', banners, name='banners'),
+    path('films/', films_list, name='films_list'),
+    path('cinemas/', cinemas_list, name='cinemas_list'),
+    path('news/', news_list, name='news_list'),
+    path('actions/', actions_list, name='actions_list'),
+    path('pages/', pages_list, name='pages_list'),
+    path('users/', users, name='users'),
+    path('mailing/', mailing, name='mailing'),
 ]
--- kinocms/cms/views.py
+++ kinocms/cms/views.py
@@ -1,6 +1,92 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 
-def index(request):
-    return HttpResponse('cms homepage')
+sidebar_pages = [
+    {'title': 'Статистика', 'url': 'statistics'},
+    {'title': 'Баннеры/Слайдеры', 'url': 'banners'},
+    {'title': 'Фильмы', 'url': 'films_list'},
+    {'title': 'Кинотеатры', 'url': 'cinemas_list'},
+    {'title': 'Новости', 'url': 'news_list'},
+    {'title': 'Акции', 'url': 'actions_list'},
+    {'title': 'Страницы', 'url': 'pages_list'},
+    {'title': 'Пользователи', 'url': 'users'},
+    {'title': 'Рассылка', 'url': 'mailing'},
+]
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def statistics(request):
+    context = {
+        'title': 'CMS | Статистика',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/statistics.html', context=context)
+
+
+def banners(request):
+    context = {
+        'title': 'CMS | Баннеры',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/banners.html', context=context)
+
+
+def films_list(request):
+    context = {
+        'title': 'CMS | Фильмы',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/films/films_list.html', context=context)
+
+
+def cinemas_list(request):
+    context = {
+        'title': 'CMS | Кинотеатры',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/cinemas/cinemas_list.html', context=context)
+
+
+def news_list(request):
+    context = {
+        'title': 'CMS | Новости',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/news/news_list.html', context=context)
+
+
+def actions_list(request):
+    context = {
+        'title': 'CMS | Акции',
+        'sidebar_pages': sidebar_pages,
+    }
+    return render(request, 'cms/actions/actions_list.html', context=context)
+
+
+def pages_list(request):
+    context = {
+        'title': 'CMS | Страницы',
+        'sidebar_pages': sidebar_pages
+    }
+    return render(request, 'cms/pages/pages_list.html', context=context)
+
+
+def users(request):
+    context = {
+        'title': 'CMS | Пользователи',
+        'sidebar_pages': sidebar_pages
+    }
+    return render(request, 'cms/users.html', context=context)
+
+
+def mailing(request):
+    context = {
+        'title': 'CMS | Рассылка',
+        'sidebar_pages': sidebar_pages
+    }
+    return render(request, 'cms/mailing.html', context=context)
