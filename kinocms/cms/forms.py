baab1209d0bf4267191160de0f8a7ba7665c985e
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-11-29 22:13:34 +0200

    Added FilmBanner (detailed) and NewsBanner (raw) Forms

--- kinocms/cms/forms.py
+++ kinocms/cms/forms.py
@@ -1,25 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.forms import ModelForm, modelformset_factory, formset_factory
-from django.forms.widgets import TextInput, PasswordInput
+from django.forms import ModelForm, modelformset_factory
+from django.forms.widgets import TextInput, PasswordInput, FileInput
 from . import models
 
 
-class CinemaForm(ModelForm):
-    class Meta:
-        model = models.Cinema
-        fields = ['name', 'description', 'city', 'address', 'map_coordinate', 'phone_number', 'email']
+FilmBannerFormSet = modelformset_factory(models.FilmBanner, fields='__all__', extra=3,
+                                         widgets={'url': TextInput(attrs={'class': 'form-control'}),
+                                                  'text': TextInput(attrs={'class': 'form-control'}),
+                                                  'image': FileInput(attrs={'class': 'form-control'})}
+                                         )
 
 
-class PageGalleryForm(ModelForm):
-    class Meta:
-        model = models.PageGallery
-        fields = ['page', 'main_image']
-
-
-PageGalleryFormSet = modelformset_factory(models.PageGallery, fields=['additional_image'], extra=5)
-
-
-class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Username', widget=TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password', widget=PasswordInput(attrs={'class': 'form-control'}))
+NewsBannerFormSet = modelformset_factory(models.NewsBanner, fields='__all__', extra=3)
