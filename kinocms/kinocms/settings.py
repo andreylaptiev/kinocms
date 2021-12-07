601812723cc06f01cc26390d1e1a8473924f7b73
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-07 18:05:36 +0200

    Added reference to my CustomUser

--- kinocms/kinocms/settings.py
+++ kinocms/kinocms/settings.py
@@ -112,11 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'cms.CustomUser'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Europe/Kiev'
 
