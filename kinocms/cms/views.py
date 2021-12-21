fba342178e6005369dec50f333c69a8775c3678b
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-21 12:21:04 +0200

    - Added function for creating film card. It creates Gallery and Seo model instances when processing form. And sets OneToOneField realation from Film to these models. Need to add Image formset. - Refactored some namings.

--- kinocms/cms/views.py
+++ kinocms/cms/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from . import forms
+from . import models
 
 
 sidebar_pages = [
     {'title': 'Статистика', 'url': 'statistics'},
     {'title': 'Баннеры/Слайдеры', 'url': 'banners'},
-    {'title': 'Фильмы', 'url': 'films_list'},
-    {'title': 'Кинотеатры', 'url': 'cinemas_list'},
+    {'title': 'Фильмы', 'url': 'film_list'},
+    {'title': 'Кинотеатры', 'url': 'cinema_list'},
     {'title': 'Новости', 'url': 'news_list'},
     {'title': 'Акции', 'url': 'actions_list'},
     {'title': 'Страницы', 'url': 'pages_list'},
@@ -58,16 +59,45 @@ def banners(request):
 
 
 @login_required(login_url='login_user')
-def films_list(request):
+def film_add(request):
+    film_form = forms.FilmForm()
+    seo_form = forms.SeoForm()
+    context = {
+        'title': 'CMS | Добавить фильм',
+        'sidebar_pages': sidebar_pages,
+        'film_form': film_form,
+        'seo_form': seo_form,
+    }
+
+    if request.method == 'POST':
+        film_form = forms.FilmForm(request.POST, request.FILES)
+        seo_form = forms.SeoForm(request.POST, request.FILES)
+        if film_form.is_valid() and seo_form.is_valid():
+            seo_form.save()
+            seo_url = seo_form.cleaned_data['url']
+            current_seo = models.Seo.objects.get(url=seo_url)
+
+            film = film_form.save(commit=False)
+            film_name = film_form.cleaned_data['name'].lower()
+            film.gallery = models.Gallery.objects.create(name=film_name)
+            film.seo = current_seo
+            film.save()
+        return redirect('film_list')
+    else:
+        return render(request, 'cms/film/film_form.html', context=context)
+
+
+@login_required(login_url='login_user')
+def film_list(request):
     context = {
         'title': 'CMS | Фильмы',
         'sidebar_pages': sidebar_pages,
     }
-    return render(request, 'cms/films/films_list.html', context=context)
+    return render(request, 'cms/film/film_list.html', context=context)
 
 
 @login_required(login_url='login_user')
-def cinemas_list(request):
+def cinema_list(request):
     context = {
         'title': 'CMS | Кинотеатры',
         'sidebar_pages': sidebar_pages,
