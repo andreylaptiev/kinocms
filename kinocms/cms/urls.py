2b46dc62c9e26506dd53aa06a719b442611b8823
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-21 12:22:16 +0200

    Refactored URL routing namings

--- kinocms/cms/urls.py
+++ kinocms/cms/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('statistics/', statistics, name='statistics'),
     path('banners/', banners, name='banners'),
-    path('films/', films_list, name='films_list'),
-    path('cinemas/', cinemas_list, name='cinemas_list'),
+    path('film_list/', film_list, name='film_list'),
+    path('film_add/', film_add, name='film_add'),
+    # path('film/<int:pk>', FilmDetailView.as_view(), name='film_detail'),
+    path('cinema_list/', cinema_list, name='cinema_list'),
     path('news/', news_list, name='news_list'),
     path('actions/', actions_list, name='actions_list'),
     path('pages/', pages_list, name='pages_list'),
