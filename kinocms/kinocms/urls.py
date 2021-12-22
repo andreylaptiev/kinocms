f9b71f32285496f71b69448a75f13f74334a9434
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-22 14:40:34 +0200

    Added link to media files.

--- kinocms/kinocms/urls.py
+++ kinocms/kinocms/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
