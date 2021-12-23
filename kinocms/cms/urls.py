cb11dd18fb179faadf223b1d222f82632cb625fe
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-23 12:43:41 +0200

    Renamed some URL paths

--- kinocms/cms/urls.py
+++ kinocms/cms/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('film_add/', film_add, name='film_add'),
     # path('film/<int:pk>', FilmDetailView.as_view(), name='film_detail'),
     path('cinema_list/', cinema_list, name='cinema_list'),
-    path('news/', news_list, name='news_list'),
-    path('actions/', actions_list, name='actions_list'),
-    path('pages/', pages_list, name='pages_list'),
+    path('news_list/', news_list, name='news_list'),
+    path('action_list/', action_list, name='actions_list'),
+    path('page_list/', page_list, name='page_list'),
     path('users/', users, name='users'),
     path('mailing/', mailing, name='mailing'),
 ]
