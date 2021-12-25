e2356c84312f108eee27d83931bdcdc5ea26882c
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-25 22:38:17 +0200

    Added function to display uploaded image

--- kinocms/cms/static/cms/js/add_image_form.js
+++ kinocms/cms/static/cms/js/add_image_form.js
@@ -1,13 +1,5 @@
 "use strict";
 
-// add empty image form
-/* currentImageFormsCount and totalImageFormsCount are different
-from banner formsets implementation because we already have
-formset_manager BEFORE adding new empty forms (because
-we add it manually to html code). In banner formsets
-formset_manager creates when we add first empty form (it creates
-with banner_formset.as_p).
-*/
 const addImageFormBtn = document.getElementById('add-image-form');
 const totalImageForms = document.getElementById('id_form-TOTAL_FORMS');
 
@@ -19,7 +11,7 @@ function add_image_form(event) {
   }
   const formCopyTarget = document.getElementById('image-formset');
   const copyEmptyImageForm = document.getElementById('image-empty-form').cloneNode(true);
-  copyEmptyImageForm.setAttribute('class', 'col-md-4 image-form');
+  copyEmptyImageForm.setAttribute('class', 'col-md-4 align-self-end text-center image-form');
   const currentImageFormsCount = document.getElementsByClassName('image-form').length;
   copyEmptyImageForm.setAttribute('id', `form-${currentImageFormsCount}`);
   const regex = new RegExp('__prefix__', 'g');
@@ -27,4 +19,14 @@ function add_image_form(event) {
   totalImageForms.setAttribute('value', currentImageFormsCount+1);
   // add empty form element to html form
   formCopyTarget.append(copyEmptyImageForm);
+  // get image field and display uploaded image
+  const imageField = document.getElementById(`id_form-${currentImageFormsCount}-image`);
+  const uploadedImage = document.getElementById(`id_form-${currentImageFormsCount}-uploaded-image`);
+  imageField.onchange = evt => {
+    const [file] = imageField.files
+    if (file) {
+      uploadedImage.src = URL.createObjectURL(file);
+      uploadedImage.setAttribute('class', '');
+    }
+  }
 }
