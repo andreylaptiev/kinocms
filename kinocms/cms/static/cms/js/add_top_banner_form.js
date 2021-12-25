98a12e4db7d8da28c27c4257755721cf63dbc068
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-25 22:34:02 +0200

    Display uploaded image function

--- kinocms/cms/static/cms/js/add_top_banner_form.js
+++ kinocms/cms/static/cms/js/add_top_banner_form.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const addTopBannerFormBtn = document.getElementById('add-top-banner-form');
-const totalTopBannerForms = document.getElementById('id_top_banners-TOTAL_FORMS');
+const totalTopBannerForms = document.getElementById('id_top_banner-TOTAL_FORMS');
 
 addTopBannerFormBtn.addEventListener('click', add_top_banner_form);
 
@@ -11,12 +11,22 @@ function add_top_banner_form(event) {
   }
   const formCopyTarget = document.getElementById('top-banner-formset');
   const copyEmptyTopBannerForm = document.getElementById('top-banner-empty-form').cloneNode(true);
-  copyEmptyTopBannerForm.setAttribute('class', 'col-6 col-md-4 top-banner-form');
+  copyEmptyTopBannerForm.setAttribute('class', 'col-md-4 align-self-end text-center top-banner-form');
   const currentTopBannerFormsCount = document.getElementsByClassName('top-banner-form').length;
-  copyEmptyTopBannerForm.setAttribute('id', `form-${currentTopBannerFormsCount-1}`);
+  copyEmptyTopBannerForm.setAttribute('id', `form-${currentTopBannerFormsCount}`);
   const regex = new RegExp('__prefix__', 'g');
-  copyEmptyTopBannerForm.innerHTML = copyEmptyTopBannerForm.innerHTML.replace(regex, currentTopBannerFormsCount-1);
-  totalTopBannerForms.setAttribute('value', currentTopBannerFormsCount);
+  copyEmptyTopBannerForm.innerHTML = copyEmptyTopBannerForm.innerHTML.replace(regex, currentTopBannerFormsCount);
+  totalTopBannerForms.setAttribute('value', currentTopBannerFormsCount+1);
   // add empty form element to html form
   formCopyTarget.append(copyEmptyTopBannerForm);
+  // get image field and display uploaded image
+  const imageField = document.getElementById(`id_top_banner-${currentTopBannerFormsCount}-image`);
+  const uploadedImage = document.getElementById(`id_top_banner-${currentTopBannerFormsCount}-uploaded-image`);
+  imageField.onchange = evt => {
+    const [file] = imageField.files
+    if (file) {
+      uploadedImage.src = URL.createObjectURL(file);
+      uploadedImage.setAttribute('class', '');
+    }
+  }
 }
