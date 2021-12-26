1f27ac822f9a31721af828c6a9ba8d9fa2962d49
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-12-26 20:47:15 +0200

    Added function to display uploaded images

--- kinocms/cms/static/cms/js/add_news_banner_form.js
+++ kinocms/cms/static/cms/js/add_news_banner_form.js
@@ -2,7 +2,7 @@
 
 // add empty news banner form
 const addNewsBannerFormBtn = document.getElementById('add-news-banner-form');
-const totalNewsBannerForms = document.getElementById('id_news_banners-TOTAL_FORMS');
+const totalNewsBannerForms = document.getElementById('id_news_banner-TOTAL_FORMS');
 
 addNewsBannerFormBtn.addEventListener('click', add_news_banner_form);
 
@@ -12,12 +12,22 @@ function add_news_banner_form(event) {
   }
   const formCopyTarget = document.getElementById('news-banner-formset');
   const copyEmptyNewsBannerForm = document.getElementById('news-banner-empty-form').cloneNode(true);
-  copyEmptyNewsBannerForm.setAttribute('class', 'col-6 col-md-4 news-banner-form');
+  copyEmptyNewsBannerForm.setAttribute('class', 'col-md-4 align-self-end text-center news-banner-form');
   const currentNewsBannerFormsCount = document.getElementsByClassName('news-banner-form').length;
-  copyEmptyNewsBannerForm.setAttribute('id', `form-${currentNewsBannerFormsCount-1}`);
+  copyEmptyNewsBannerForm.setAttribute('id', `form-${currentNewsBannerFormsCount}`);
   const regex = new RegExp('__prefix__', 'g');
-  copyEmptyNewsBannerForm.innerHTML = copyEmptyNewsBannerForm.innerHTML.replace(regex, currentNewsBannerFormsCount-1);
-  totalNewsBannerForms.setAttribute('value', currentNewsBannerFormsCount);
+  copyEmptyNewsBannerForm.innerHTML = copyEmptyNewsBannerForm.innerHTML.replace(regex, currentNewsBannerFormsCount);
+  totalNewsBannerForms.setAttribute('value', currentNewsBannerFormsCount+1);
   // add empty form element to html form
   formCopyTarget.append(copyEmptyNewsBannerForm);
+  // get image field and display uploaded image
+  const imageField = document.getElementById(`id_news_banner-${currentNewsBannerFormsCount}-image`);
+  const uploadedImage = document.getElementById(`id_news_banner-${currentNewsBannerFormsCount}-uploaded-image`);
+  imageField.onchange = evt => {
+    const [file] = imageField.files
+    if (file) {
+      uploadedImage.src = URL.createObjectURL(file);
+      uploadedImage.setAttribute('class', '');
+    }
+  }
 }
